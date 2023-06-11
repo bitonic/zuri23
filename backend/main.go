d0843abac730b536c4acc90b26bc287e7cab3dbc
Jussi Maki <joamaki@gmail.com>
2023-06-11 15:18:13 +0300

    Puzzle state and state sharing with the client

--- backend/main.go
+++ backend/main.go
@@ -2,26 +2,154 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"os/exec"
+	"strings"
+	"time"
+
+	"golang.org/x/exp/slices"
 )
 
 func main() {
+	s := &puzzleState{
+		currentPuzzle: 0,
+		ghciOut:       "<n/a>",
+		tokens:        slices.Clone(puzzles[0]),
+	}
+	go s.run()
+
 	http.HandleFunc("/post", handlePost)
 	http.Handle("/", http.FileServer(http.Dir("../frontend")))
 	http.ListenAndServe("0.0.0.0:8001", http.DefaultServeMux)
 }
 
+type tokenLoc struct {
+	X, Y float64
+}
+
+type puzzleToken struct {
+	tokenLoc
+	Token     string
+	imageFile string
+}
+
+func (t puzzleToken) token() string {
+	return t.Token
+}
+
+func (t puzzleToken) Loc() tokenLoc {
+	return t.tokenLoc
+}
+
+type puzzle []puzzleToken
+
+type clientUpdate struct {
+	puzzleID int
+	clientID int
+	loc      tokenLoc
+	response chan postResponse
+}
+
+func mk(tokens ...string) puzzle {
+	return Map(tokens, func(t string) puzzleToken {
+		return puzzleToken{
+			tokenLoc: tokenLoc{50, 50},
+			Token:    t,
+		}
+	})
+}
+
+var (
+	puzzles = []puzzle{
+		mk("foo", "bar", "baz"),
+	}
+
+	updates = make(chan clientUpdate, 32)
+)
+
+type puzzleState struct {
+	currentPuzzle int
+	ghciOut       string
+	tokens        []puzzleToken
+}
+
+func Map[A, B any](xs []A, f func(A) B) []B {
+	out := make([]B, len(xs))
+	for i := range xs {
+		out[i] = f(xs[i])
+	}
+	return out
+}
+
+func arrange(tokens []puzzleToken) []puzzleToken {
+	tokens = slices.Clone(tokens)
+	slices.SortFunc(tokens, func(a, b puzzleToken) bool {
+		return a.X < b.X
+	})
+	return tokens
+}
+
+func (s *puzzleState) run() {
+	ticks := time.NewTicker(100 * time.Millisecond)
+	//newRoundCountdown := 50
+
+	for {
+		select {
+		case u := <-updates:
+			if u.puzzleID == s.currentPuzzle && u.clientID >= 0 && u.clientID < len(s.tokens) {
+				log.Printf("update[%d]: %+v", u.clientID, u.loc)
+				s.tokens[u.clientID].tokenLoc = u.loc
+			}
+			u.response <- postResponse{
+				GHCIOutput: s.ghciOut,
+				Tokens:     slices.Clone(s.tokens),
+			}
+			close(u.response)
+
+		case <-ticks.C:
+			tokens := arrange(s.tokens)
+			expr := strings.Join(
+				Map(tokens, puzzleToken.token),
+				" ")
+			s.ghciOut = evaluate(expr)
+			//log.Printf("evaluate(%s): %s", expr, s.ghciOut)
+
+			/*
+				if slices.Equal(s.tokens, tokens) {
+					newRoundCountdown--
+					if newRoundCountdown == 0 {
+						newRoundCountdown = 50
+						s.next()
+					}
+				}*/
+
+		}
+	}
+}
+
+func (s *puzzleState) next() bool {
+	s.currentPuzzle += 1
+	if s.currentPuzzle >= len(puzzles) {
+		return false
+	}
+
+	s.tokens = slices.Clone(puzzles[s.currentPuzzle])
+	return true
+}
+
 type postRequest struct {
-	Txt string  `json:"txt"`
-	X   float64 `json:"x"`
-	Y   float64 `json:"y"`
+	ClientID int
+	PuzzleID int
+	X        float64
+	Y        float64
 }
 
 type postResponse struct {
 	GHCIOutput string
+	PuzzleID   int
+	Tokens     []puzzleToken
 }
 
 func handlePost(w http.ResponseWriter, req *http.Request) {
@@ -33,15 +161,21 @@ func handlePost(w http.ResponseWriter, req *http.Request) {
 	if err != nil {
 		panic(err)
 	}
+	log.Printf("request: %+v", postReq)
 
-	fmt.Printf(">>> %+v\n", postReq)
-
-	out := runGhci("aoeu")
-	resp, _ := json.Marshal(postResponse{out})
-	w.Write(resp)
+	resp := make(chan postResponse, 1)
+	updates <- clientUpdate{
+		puzzleID: 0,
+		clientID: postReq.ClientID,
+		loc:      tokenLoc{postReq.X, postReq.Y},
+		response: resp,
+	}
+	respJson, _ := json.Marshal(<-resp)
+	log.Printf("responding: %q", respJson)
+	w.Write(respJson)
 }
 
-func runGhci(input string) string {
+func evaluate(input string) string {
 	out, err := exec.Command("/usr/bin/ghci", "-e", input).CombinedOutput()
 	if err != nil {
 		return string(out) + "(" + err.Error() + ")"
--- frontend/script.js
+++ frontend/script.js
@@ -1,36 +1,62 @@
 const canvas = document.getElementById("notebook");
 canvas.style.cursor = 'none';
 const ctx = canvas.getContext("2d");
-const tokens = [
+/*const tokens = [
     { txt: "iterate", x: 50 + Math.random() * 100, y: 50 + Math.random() * 500 },
     { txt: "take",    x: 50 + Math.random() * 100, y: 50 + Math.random() * 500 },
     { txt: "10",      x: 50 + Math.random() * 100, y: 50 + Math.random() * 500 },
     { txt: "$",       x: 50 + Math.random() * 100, y: 50 + Math.random() * 500 },
     { txt: "(+1)",    x: 50 + Math.random() * 100, y: 50 + Math.random() * 500 },
     { txt: "0",       x: 50 + Math.random() * 100, y: 50 + Math.random() * 500 },
-];
+];*/
+
+var tokens = [
+    { txt: "...", X: 50 + Math.random() * 100, Y: 50 + Math.random() * 500 },
+]
+
 ctx.fillStyle = "rgb(0, 100, 100)";
 ctx.font = "30px monospace";
-const clientId = Math.floor(Math.random() * tokens.length);
+var clientId = 0;
+var puzzleId = -1;
+
 canvas.addEventListener("mousemove", e => {
     const rect = canvas.getBoundingClientRect();
-    tokens[clientId].x = e.pageX - rect.left;
-    tokens[clientId].y = e.pageY - rect.top;
+    tokens[clientId].X = e.pageX - rect.left;
+    tokens[clientId].Y = e.pageY - rect.top;
+    render();
+});
+
+function render() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     for (let i in tokens) {
         const token = tokens[i];
-        ctx.fillText(token.txt, token.x, token.y);
+        ctx.fillText(token.Token, token.X, token.Y);
     }
-});
+}
+
 async function send() {
     try {
         const result = await fetch('/post', {
             method: 'POST',
-            body: JSON.stringify(tokens[clientId]),
+            body: JSON.stringify({
+	            "ClientID": clientId,
+	            "PuzzleID": puzzleId,
+	            "X": tokens[clientId].X,
+	            "Y": tokens[clientId].Y}),
         });
         const obj = await result.json();
+        tokens = obj.Tokens;
+
+        if (obj.PuzzleID > puzzleId) {
+	        // New puzzle!
+	        puzzleId = obj.PuzzleID;
+	        clientId = Math.floor(Math.random() * tokens.length);
+
+	        console.log("clientId: ", clientId);
+        }
         console.log(obj);
         document.getElementById("ghci").innerHTML = obj.GHCIOutput;
+        render();
     } finally {
         setTimeout(send, 100);
     }
