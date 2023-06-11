4e498853f19700861da73a32e0fd41fdace32322
Alex Sayers <alex@asayers.com>
2023-06-11 19:13:26 +0900

    Add initial frontend prototype

---
+++ frontend/script.js
@@ -0,0 +1,33 @@
+const canvas = document.getElementById("notebook");
+canvas.style.cursor = 'none';
+const ctx = canvas.getContext("2d");
+const tokens = [
+    { txt: "iterate", x: 50 + Math.random() * 100, y: 50 + Math.random() * 500 },
+    { txt: "take",    x: 50 + Math.random() * 100, y: 50 + Math.random() * 500 },
+    { txt: "10",      x: 50 + Math.random() * 100, y: 50 + Math.random() * 500 },
+    { txt: "$",       x: 50 + Math.random() * 100, y: 50 + Math.random() * 500 },
+    { txt: "(+1)",    x: 50 + Math.random() * 100, y: 50 + Math.random() * 500 },
+    { txt: "0",       x: 50 + Math.random() * 100, y: 50 + Math.random() * 500 },
+];
+ctx.fillStyle = "rgb(0, 100, 100)";
+ctx.font = "30px monospace";
+const clientId = Math.floor(Math.random() * tokens.length);
+canvas.addEventListener("mousemove", e => {
+    const rect = canvas.getBoundingClientRect();
+    tokens[clientId].x = e.pageX - rect.left;
+    tokens[clientId].y = e.pageY - rect.top;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    for (let i in tokens) {
+        const token = tokens[i];
+        ctx.fillText(token.txt, token.x, token.y);
+    }
+});
+function send() {
+    fetch('http://152.96.94.193:8001', {
+        method: 'POST',
+        body: JSON.stringify(tokens[clientId]),
+    })
+    .then(x => document.getElementById("ghci").innerHTML = x.json())
+    .finally(() => setTimeout(send, 100));
+}
+send();
